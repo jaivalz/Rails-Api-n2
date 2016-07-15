b580352286dcc580cdd8fad3648a726ccf4c8011
Jaime <jaivalz@yahoo.com>
2016-07-15 12:47:25 +0000

    Created manufacturers class to inherit from grape api, first touch with postman

---
+++ lib/api/v1/manufacturers.rb
@@ -0,0 +1,27 @@
+module Api
+  module V1
+    class Manufacturers < Grape::API
+
+      resource :manufacturers do
+
+        desc "Return a list of manufacturers."
+        get do
+          manufacturers = Manufacturer.all
+          present manufacturers
+        end
+        
+        desc "Return a manufacturer."
+        params do
+          requires :id, type: Integer, desc: "Manufacturer id."
+        end
+        route_param :id do
+          get do
+            manufacturer = Manufacturer.find(params[:id])
+            present manufacturer
+          end
+        end
+        
+      end
+    end
+  end
+end
